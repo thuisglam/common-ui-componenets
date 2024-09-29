7ac036a6ac5379e7c31f55c8a958e1ebcef0a697
sudheerkb <sudheerec107@gmail.com>
2024-09-29 09:36:32 -0400

    feat: added cdk

---
+++ cdk/lib/website-stack.ts
@@ -0,0 +1,211 @@
+import * as cdk from "aws-cdk-lib";
+import {
+  CachePolicy,
+  CfnDistribution,
+  CfnOriginAccessControl,
+  CloudFrontAllowedCachedMethods,
+  CloudFrontAllowedMethods,
+  CloudFrontWebDistribution,
+  OriginRequestPolicy,
+  ViewerProtocolPolicy,
+} from "aws-cdk-lib/aws-cloudfront";
+import { Effect, PolicyStatement, ServicePrincipal } from "aws-cdk-lib/aws-iam";
+import {
+  ARecord,
+  AaaaRecord,
+  HostedZone,
+  RecordTarget,
+} from "aws-cdk-lib/aws-route53";
+import { CloudFrontTarget } from "aws-cdk-lib/aws-route53-targets";
+import {
+  BlockPublicAccess,
+  Bucket,
+  BucketEncryption,
+  ObjectOwnership,
+} from "aws-cdk-lib/aws-s3";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+import { Construct } from "constructs";
+import { websiteConfiguration } from "../config/websiteConfiguration";
+
+const path = "../website/dist/provider-portal/browser";
+
+interface WebSiteStackProps extends cdk.StackProps {
+  certificateArnMap: Map<string, string>; // Add the certificateArnMap to props
+}
+
+export class WebSiteStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: WebSiteStackProps) {
+    super(scope, id, props);
+
+    // Create an S3 bucket to host the static website
+    const hostingBucket = new Bucket(this, "provider-portal-website", {
+      autoDeleteObjects: true,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      encryption: BucketEncryption.S3_MANAGED,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // Deploy website assets to the S3 bucket
+    new BucketDeployment(this, "provider-portal-website-deployment", {
+      sources: [Source.asset(path)],
+      destinationBucket: hostingBucket,
+    });
+
+    // Create an Origin Access Control (OAC) to securely allow CloudFront to access the S3 bucket
+    const oac = new CfnOriginAccessControl(this, "provider-portal-oac", {
+      originAccessControlConfig: {
+        name: "provider-portal-oac",
+        originAccessControlOriginType: "s3",
+        signingBehavior: "always",
+        signingProtocol: "sigv4",
+      },
+    });
+
+    // Create an S3 bucket for CloudFront access logging
+    const distributionLoggingPrefix = "distribution-access-logs/";
+    const distributionLoggingBucket = new Bucket(
+      this,
+      "provider-portal-distribution-logging-bucket",
+      {
+        objectOwnership: ObjectOwnership.OBJECT_WRITER,
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+        autoDeleteObjects: true,
+        blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+        publicReadAccess: false,
+        lifecycleRules: [
+          {
+            prefix: distributionLoggingPrefix,
+            abortIncompleteMultipartUploadAfter: cdk.Duration.days(30),
+            expiration: cdk.Duration.days(30),
+          },
+        ],
+      }
+    );
+
+    // Retrieve domain configurations from the websiteConfiguration
+    const domainConfigs = websiteConfiguration.domains;
+
+    domainConfigs.forEach((domainConfig, index) => {
+      // Configure API origin for CloudFront
+      const apiOriginConfig = {
+        customOriginSource: {
+          domainName: `${domainConfig.apiId}.execute-api.${this.region}.amazonaws.com`,
+          originPath: "", // Adjust to your API stage if different
+        },
+        behaviors: [
+          {
+            pathPattern: "/prod/*",
+            allowedMethods: CloudFrontAllowedMethods.ALL,
+            cachePolicy: CachePolicy.CACHING_DISABLED,
+            compress: true,
+            cachedMethods: CloudFrontAllowedCachedMethods.GET_HEAD_OPTIONS,
+            viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+            minTtl: cdk.Duration.seconds(0),
+            maxTtl: cdk.Duration.seconds(86400),
+            defaultTtl: cdk.Duration.seconds(3600),
+            originRequestPolicy:
+              OriginRequestPolicy.ALL_VIEWER_EXCEPT_HOST_HEADER
+          },
+        ],
+      };
+
+      // Create CloudFront distributions for each subdomain
+      const domainsToHandle = [...domainConfig.subdomains.map(sub => `${sub}.${domainConfig.domainName}`)];
+      domainsToHandle.forEach((fullDomain, subIndex) => {
+
+        // Use the certificateArnMap to fetch the certificate ARN
+        const certificateArn = props.certificateArnMap.get(fullDomain);
+
+        if (!certificateArn) {
+          throw new Error(`Certificate ARN not found for domain: ${fullDomain}`);
+        }
+
+        // Create CloudFront distribution
+        const cloudFrontDistribution = new CloudFrontWebDistribution(
+          this,
+          `provider-portal-distribution-${index}-${subIndex}`,
+          {
+            originConfigs: [
+              {
+                s3OriginSource: {
+                  s3BucketSource: hostingBucket,
+                },
+                behaviors: [
+                  {
+                    isDefaultBehavior: true,
+                    allowedMethods: CloudFrontAllowedMethods.GET_HEAD,
+                    compress: true,
+                    cachedMethods: CloudFrontAllowedCachedMethods.GET_HEAD,
+                    viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                    minTtl: cdk.Duration.seconds(0),
+                    maxTtl: cdk.Duration.seconds(86400),
+                    defaultTtl: cdk.Duration.seconds(3600),
+                  },
+                ],
+              },
+              apiOriginConfig,
+            ],
+            viewerCertificate: {
+              aliases: [fullDomain],
+              props: {
+                acmCertificateArn: certificateArn, // Use the certificateArn from the map
+                sslSupportMethod: "sni-only",
+                minimumProtocolVersion: "TLSv1.2_2021",
+              },
+            },
+            loggingConfig: {
+              bucket: distributionLoggingBucket,
+              includeCookies: false,
+            },
+            defaultRootObject: "index.html"
+          }
+        );
+
+        // Work around for OAC to add property override to the CloudFront distribution
+        const cfnDistribution = cloudFrontDistribution.node
+          .defaultChild as CfnDistribution;
+
+        cfnDistribution.addPropertyOverride(
+          "DistributionConfig.Origins.0.OriginAccessControlId",
+          oac.getAtt("Id")
+        );
+
+        // Create CloudFront target for Route 53
+        const cdfTarget = new CloudFrontTarget(cloudFrontDistribution);
+
+        // Lookup the hosted zone for the domain and create unique IDs for each
+        const hostedZone = HostedZone.fromLookup(this, `HostedZone-${domainConfig.domainName}-${subIndex}`, {
+          domainName: fullDomain,
+        });
+
+        // Create DNS records for the domain and subdomains
+        new ARecord(this, `PublicARecord-${index}-${subIndex}`, {
+          recordName: fullDomain,
+          zone: hostedZone,
+          target: RecordTarget.fromAlias(cdfTarget),
+        });
+
+        new AaaaRecord(this, `PublicAAARecord-${index}-${subIndex}`, {
+          recordName: fullDomain,
+          zone: hostedZone,
+          target: RecordTarget.fromAlias(cdfTarget),
+        });
+
+        // Add a policy to the S3 bucket to allow CloudFront to access it
+        hostingBucket.addToResourcePolicy(
+          new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: ["s3:GetObject"],
+            principals: [new ServicePrincipal("cloudfront.amazonaws.com")],
+            resources: [`${hostingBucket.bucketArn}/*`],
+            conditions: {
+              StringEquals: {
+                "AWS:SourceArn": `arn:aws:cloudfront::${props?.env?.account}:distribution/${cloudFrontDistribution.distributionId}`,
+              },
+            },
+          })
+        );
+      });
+    });
+  }
+}
--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { createCustomElement } from '@angular/elements';
+import { FooterComponent } from './footer/footer.component';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './i18n/', '.json');
@@ -15,6 +16,7 @@ export function HttpLoaderFactory(http: HttpClient) {
 @NgModule({
   declarations: [],
   imports: [
+    FooterComponent,
     HeaderComponent,
     CommonModule,
     BrowserModule,
@@ -36,7 +38,11 @@ export class AppModule implements DoBootstrap {
     const headerComponent = createCustomElement(HeaderComponent, {
       injector: this.injector
     });
+    const footerComponent = createCustomElement(FooterComponent, {
+      injector: this.injector
+    });
     customElements.define('thuis-header', headerComponent);
+    customElements.define('thuis-footer', footerComponent);
     // appRef.bootstrap(HeaderComponent);
   }
 
